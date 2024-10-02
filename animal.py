73b018e23833f90c726d0a7b954b37300ad01566
Valerioe1 <valerioe@oregonstate.edu>
2024-10-02 15:42:38 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,9 @@
+
+
+# Name: Edgar Valerio
+# GiftHub Username: Valerioe1
+# Date: 10/02/24
+# Description: Asks the user for their favorite animal and then prints out that, that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("your favorite animal is the " + fave_animal + ".")
